10e8852ecd08527b6b8f8539cf875bab1bae8a96
UP2005572 <91529143+UP2005572@users.noreply.github.com>
2021-12-14 16:55:12 +0000

    linked game activity to start

--- Project/app/src/main/java/com/example/honeycombmaze/MainActivity.java
+++ Project/app/src/main/java/com/example/honeycombmaze/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.honeycombmaze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.content.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (ImageView) findViewById(R.id.hexagon);
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,GameActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
