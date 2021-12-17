137c6a14c38a457008610098db4c5658855a1234
UP2005572 <91529143+UP2005572@users.noreply.github.com>
2021-12-17 23:12:58 +0000

    link intro to game

--- Project/app/src/main/java/com/example/honeycombmaze/IntroScreen.java
+++ Project/app/src/main/java/com/example/honeycombmaze/IntroScreen.java
@@ -2,7 +2,10 @@ package com.example.honeycombmaze;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class IntroScreen extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class IntroScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_screen);
+
+        ImageView startButton;
+
+        startButton = (ImageView) findViewById(R.id.start);
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent startToGame = new Intent(IntroScreen.this, InChamberActivity.class);
+                startActivity(startToGame);
+            }
+        });
     }
 }
